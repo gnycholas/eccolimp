98f5b8872d33ef24ef5574a10828521d1c3f5a1c
Gustavo Nycholas <gustavonycholas@gmail.com>
2024-12-17 20:47:17 -0300

    Initial commit

---
+++ src/main/java/com/vale/eccolimp/EccolimpApplication.java
@@ -0,0 +1,13 @@
+package com.vale.eccolimp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EccolimpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EccolimpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vale/eccolimp/EccolimpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vale.eccolimp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EccolimpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
